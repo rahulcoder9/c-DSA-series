18aae44b8283dd5991c44bf757cd80ade5364ea9
Rahul Prasad <>
2022-12-24 10:34:34 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@samle/day1.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main()
+{ 
+  /*
+    int a;
+    char ch= 'w';
+    bool b1= true ;
+    float f= 1.8 ;
+    double  d = 1.256 ;
+    cout<<"enter a no";
+    cin>>a;
+    cout<<a<<ends<<"hello world"<<endl<<"thankyou\n"<<ch<<"\n";
+    cout<<b1;
+    cout<<f<<endl;
+    cout<<d<<endl;
+
+    int size = sizeof(ch);
+    cout<<"Size of a is :" <<size<<endl;
+    return 0;
+  */
+
+    int a = 'a';
+    cout<<a<<endl;
+    char ch = 98 ;
+    cout<<ch<<endl;
+}
---
+++ Datatype_variable_operator.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+int main() {
+  /*
+    int a;
+    char ch= 'w';
+    bool b1= true ;
+    float f= 1.8 ;
+    double  d = 1.256 ;
+    cout<<"enter a no";
+    cin>>a;
+    cout<<a<<ends<<"hello world"<<endl<<"thankyou\n"<<ch<<"\n";
+    cout<<b1;
+    cout<<f<<endl;
+    cout<<d<<endl;
+
+    int size = sizeof(ch);
+    cout<<"Size of a is :" <<size<<endl;
+    return 0;
+
+
+    int a = 'a';
+    cout<<a<<endl;
+    char ch = 98 ;
+    cout<<ch<<endl;
+    char a1=123456;
+    cout<<a1<<endl;
+
+
+
+    unsigned int a = -12345;
+    cout<<a;
+
+    int a = 2/5 ;
+    cout<<a<<endl;
+
+    float a1 = 2.0/5 ;
+    cout<<a1<<endl;
+
+  */
+  int a = 2;
+  int b = 5;
+
+  bool first = (a == b);
+  cout << first << endl;
+
+  int a1 = 2;
+  int b1 = 5;
+
+  bool first1 = (a1 < b1);
+  cout << first1 << endl;
+}
